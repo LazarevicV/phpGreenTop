41acde20bd51ffc8c279e92dcc4a2cf3030ba082
LazarevicV <vukadinlazarevic@gmail.com>
2023-03-24 02:39:42 +0100

    nothing much changed but title

--- nova_stranica.php
+++ nova_stranica.php
@@ -1,11 +1,11 @@
 <?php
 
-$title = "nova stranica";
+$GLOBALS['title'] = "nova stranica";
 include "modules/header.php";
 include "modules/mainnav.php";
 
 ?>
 
-<h1><?=$title?></h1>
+<h1><?=$GLOBALS['title']?></h1>
 
 <?php include "modules/footer.php" ?>
